e8a879dac737a1e7456caf7dcc32a24c6013d910
justin-russell <38231076+justin-russell@users.noreply.github.com>
2021-02-19 16:06:30 -0600

    Register
    Created register page
    Created login page (NOT FINISHED)

--- Tweeter/project/views.py
+++ Tweeter/project/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def front(request):
@@ -8,4 +10,13 @@ def login(request):
     return render(request, 'project/login.html')
 
 def register(request):
-    return render(request, 'project/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')  # get username
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'project/register.html', {'form': form})
