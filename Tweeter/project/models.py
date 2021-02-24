207c97362372243238169baf4f9eeb67326478d4
Lucas Roberts <72527856+lpr70@users.noreply.github.com>
2021-02-24 17:40:36 -0600

    Post table
    made Post table in database

--- Tweeter/project/models.py
+++ Tweeter/project/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+class Post(models.Model):
+    content = models.TextField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField()
 
-# Create your models here.
+    def __str__(self):
+        return self.content
