ec8c8cfa22e4be1834448a2b599905e13ce77260
mcPython04 <74746120+mcPython04@users.noreply.github.com>
2021-03-25 22:27:11 -0500

    Commenting on Post
    Implemented comments on post

--- Tweeter/accounts/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountsConfig(AppConfig):
-    name = 'accounts'
-
-    # how signals are used in the project
-    def ready(self):
-        import accounts.signals
--- Tweeter/project/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Post, Profile
-
-# This is the admin page where the admin can see the profiles and posts
-admin.site.register(Post)
-admin.site.register(Profile)
